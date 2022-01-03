42b10cfdff587c4287150917ae71d78dd99f1a31
daibingh <daibingh@gmail.com>
2022-01-03 12:34:48 +0800

    first

---
+++ src/extension.ts
@@ -0,0 +1,17 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    
+    console.log('Congratulations, your extension "runpy" is now active!');
+
+    let disposable = vscode.commands.registerCommand('runpy.helloWorld', () => {
+        vscode.window.showInformationMessage('Hello World from RunPy!');
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
